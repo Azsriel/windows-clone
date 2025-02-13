b6afa849f0dda459987ba833564b9ff025227af6
Syed Furqaan Lateef <sfurqaanl@gmail.com>
2025-02-13 18:42:07 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Static Website</h1>
+      <p>This is the starting point of my React project.</p>
+    </div>
+  );
+}
+
+export default App;
